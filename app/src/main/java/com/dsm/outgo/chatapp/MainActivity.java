28e8618cc177a35147537b2429fbf19ef46721e9
GANESH <giri.ganesh337@gmail.com>
2016-08-31 16:47:17 +0530

    Log Msg Addes

--- app/src/main/java/com/dsm/outgo/chatapp/MainActivity.java
+++ app/src/main/java/com/dsm/outgo/chatapp/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // get our input field by its ID
         messageInput = (EditText) findViewById(R.id.message_input);
 
-        Log.d("Github Changes","Changes");
+        Log.d("Github Changes","Change");
         // get our button by its ID
         sendButton = (Button) findViewById(R.id.send_button);
 
